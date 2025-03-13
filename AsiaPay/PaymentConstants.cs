9dab2867defb850c397e28e550eb5b819536be2a
pshtiwan-hassan <67946816+pshtiwan-hassan@users.noreply.github.com>
2025-03-13 10:35:05 +0300

    Create PaymentConstants.cs

---
+++ AsiaPay/PaymentConstants.cs
@@ -0,0 +1,14 @@
+namespace AsiaPayPaymentSDK.AsiaPay
+{
+    public static class PaymentConstants
+    {
+        public const string CheckUrl = "https://apitest.asiapay.iq:5443/payment/web/paygate";
+        public const string ServerUrl = "https://apitest.asiapay.iq:5443/apiaccess";
+        public const string CheckH5MidPageUrl = "https://apitest.asiapay.iq:5443/demo/middle_page/index.html?businessType=CrossAppPay";
+        public const string AppSecret = "AppSecret";
+        public const string XAppKey = "XAppKey";
+        public const string AppId = "AppId";
+        public const string? MerchCode = "MerchCode";
+        public const string PrivateKey = "PrivateKey";
+    }
+}
